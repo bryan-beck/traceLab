22016a78c16eb8860fed8ac190ca0ae134007f8d
bryan-beck <bdbeck89@gmail.com>
2021-09-14 13:05:25 -0700

    all

--- index.js
+++ index.js
@@ -8,17 +8,32 @@ let rollbar = new Rollbar({
     captureUncaught: true,
     captureUnhandledRejections: true
 })
-const students = [];
+const colleges = [];
 const app = express();
 app.use(express.json());
 // app.get("/", (req, res) => {
 //         res.sendFile(path.join(__dirname, "/public/index.html"));
 //         rollbar.info("HTML file served successfully!");
     // });
-    // app.post('/api/student', (req, res) => {
-    //     let {name} = req.body;
-    //     name = name.trim();
-    //     students.push(name);
+    app.post('/api/college', (req, res)=>{
+        let {name} = req.body
+        name = name.trim()
+    
+        const index = colleges.findIndex(collegeName=> collegeName === name)
+    
+        if(index === -1 && name !== ''){
+            students.push(name)
+            rollbar.log('College has been added successfully', {author: 'Bryan B', type: 'manual entry'})
+            res.status(200).send(colleges)
+        } else if (name === ''){
+            rollbar.error('No name given')
+            res.status(400).send('must provide a name.')
+        } else {
+            rollbar.error('student already exists')
+            res.status(400).send('that student already exists')
+        }
+    
+    })
 
         rollbar.log("Hello World");
         // res.status(200).send(students);
